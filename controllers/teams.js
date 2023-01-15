ac68fc2b461b8228d0d40c0e8503203bb46e715b
Drew Neece <dneece5@aggienetwork.com>
2023-01-15 16:01:21 -0600

    create 'create' functionality with the ability to add a team to the database

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,9 +1,23 @@
+import { Team } from '../models/team.js'
+
 function newTeam(req, res) {
   res.render('teams/new', {
     title: 'Add Team'
   })
 }
 
+function create(req, res) {
+  Team.create(req.body)
+  .then(team => {
+    res.redirect('/teams/new')
+  })
+  .catch(err => {
+    console.error(err)
+    res.redirect('/teams/new')
+  })
+}
+
 export {
   newTeam as new,
+  create,
 }
--- routes/teams.js
+++ routes/teams.js
@@ -6,6 +6,9 @@ const router = Router()
 // GET localhost:3000/teams/new
 router.get('/new', teamsCtrl.new)
 
+// POST localhost:3000/teams
+router.post('/', teamsCtrl.create)
+
 export {
   router
 }
