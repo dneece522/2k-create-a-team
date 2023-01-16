2998b1bda3a6b2066ff73236412b52ddc15e6120
Drew Neece <dneece5@aggienetwork.com>
2023-01-16 11:00:10 -0600

    add new Player model

---
+++ models/player.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const playerSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  position: {
+    type: String,
+    enum: ['Point Guard', 'Shooting Guard', 'Small Forward', 'Power Forward', 'Center']
+  },
+  college: {
+    type: String,
+    required: true
+  },
+  offRating: {
+    type: String,
+    min: 0,
+    max: 99
+  },
+  defRating: {
+    type: String,
+    min: 0,
+    max: 99
+  }
+}, {
+  timestamps: true
+})
+
+const Player = mongoose.model('Team', playerSchema)
+
+export {
+  Player
+}
